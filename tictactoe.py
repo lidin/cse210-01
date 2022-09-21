95eff093f643af870ca3509d5b398c972088835a
Angela Lidin <angela@angelalidin.com>
2022-09-21 22:35:43 +0200

    added the tictactoe py file

---
+++ tictactoe.py
@@ -0,0 +1,9 @@
+"""CSE 210, W02 Solo Checkpoint: Tic-Tac-Toe, Angela Lidin."""
+
+"""The program must have at least one if/then block"""
+"""The program must have at least one while loop"""
+"""The program must have more than one function"""
+"""The program must have a function called main"""
+
+
+def main()
